3d4a035f3ca3907ebc28a916a40f05e0488b93a6
gmartm08 <gmartm08@estudiantes.unileon.es>
2025-04-29 17:48:46 +0000

    fix: adjust canvas to middle width and create publish button

--- frontend/src/pages/CanvasPage.tsx
+++ frontend/src/pages/CanvasPage.tsx
@@ -7,29 +7,36 @@ function CanvasPage() {
 
     return (
         <div className="flex flex-row">
-            <div className="aside flex flex-col flex-2">
-                <button className="btn btn-primary">Borrar</button>
-                <button className="btn btn-primary">Lápiz</button>
+            <div className="aside flex flex-col flex-1">
+                <button onClick={() => {setTool("eraser")}} className="btn btn-circle">Borrar</button>
                 
                 
-                <button className="btn btn-circle border-b-neutral-50 bg-red-500"></button>
-                <button className="btn btn-circle bg-orange-400"></button>
-                <button className="btn btn-circle bg-yellow-300"></button>
-                <button className="btn btn-circle bg-green-400"></button>
-                <button className="btn btn-circle bg-blue-500"></button>
-                <button className="btn btn-circle bg-purple-500"></button>
-                <button className="btn btn-circle bg-black"></button>
+                <button onClick={() => {setColor("")}} className="btn btn-circle border-b-neutral-50 bg-red-500"></button>
+                <button onClick={() => {setColor("")}}className="btn btn-circle bg-orange-400"></button>
+                <button onClick={() => {setColor("")}} className="btn btn-circle bg-yellow-300"></button>
+                <button onClick={() => {setColor("")}} className="btn btn-circle bg-green-400"></button>
+                <button onClick={() => {setColor("")}} className="btn btn-circle bg-blue-500"></button>
+                <button onClick={() => {setColor("")}} className="btn btn-circle bg-purple-500"></button>
+                <button onClick={() => {setColor("#000000")}} className="btn btn-circle bg-black"></button>
                 
-                <button className="btn btn-circle bg-gradient-to-r from-yellow-500 via-green-400 via-blue-500 to-purple-500 rainbow-animated"></button>
+                <button  className="btn btn-circle bg-gradient-to-r from-yellow-500 via-green-400 via-blue-500 to-purple-500 rainbow-animated"></button>
 
-                
+            </div>
+
+            <div className="flex flex-col flex-2">
+                <div className="canvas-container border-2">
+                    <canvas id="canvas" width="800" height="800"></canvas>
+                </div>
+
+                <footer className="footer justify-items-center">
+                    <form className="form">
+                        <button type="submit" className="btn btn-primary">Publicar</button>
+                    </form>
+                </footer>
 
             </div>
 
             <div className="flex flex-1">
-                <div className="canvas-container border-2">
-                    <canvas id="canvas" width="800" height="800"></canvas>
-                </div>
 
             </div>
 
