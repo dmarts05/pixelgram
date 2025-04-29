edf755bf7476afb182ed6d2677310f00bdd35584
dmarts05 <dmarts05@estudiantes.unileon.es>
2025-04-29 17:05:03 +0000

    chore: add production backend url to frontend

--- frontend/src/utils/constants.ts
+++ frontend/src/utils/constants.ts
@@ -1,2 +1,4 @@
 export const NAVBAR_HEIGHT = "4.5rem";
-export const API_URL = import.meta.env.DEV ? "http://localhost:8000" : "";
+export const API_URL = import.meta.env.DEV
+    ? "http://localhost:8000"
+    : "https://pixelgram-backend.onrender.com";
