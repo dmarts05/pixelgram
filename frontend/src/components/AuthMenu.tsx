395ef31875a16cff8ccd359307f6aa5f6cf4bf04
dmarts05 <dmarts05@estudiantes.unileon.es>
2025-05-04 18:15:15 +0000

    feat: make navbar responsive and add canvas and feed links

--- frontend/src/components/AuthMenu.tsx
+++ frontend/src/components/AuthMenu.tsx
@@ -1,40 +1,42 @@
+import { useMutation } from "@tanstack/react-query";
 import { Link, useNavigate } from "react-router";
 import { logout } from "../services/auth-service";
 import { useAuthStore } from "../stores/auth-store";
 
-function NavLinks(): React.ReactNode {
+function AuthMenu(): React.ReactNode {
     const navigate = useNavigate();
-
     const { isAuthenticated, setIsAuthenticated } = useAuthStore();
+
+    const mutation = useMutation({
+        mutationFn: logout,
+        onSuccess: () => {
+            setIsAuthenticated(false);
+            navigate("/");
+        },
+    });
 
     return (
         <div className="flex items-center gap-2">
-            {!isAuthenticated && (
+            {isAuthenticated ? (
+                <button
+                    className="btn btn-secondary"
+                    onClick={() => mutation.mutate()}
+                    disabled={mutation.isPending}
+                >
+                    Log Out
+                </button>
+            ) : (
                 <>
                     <Link to="/auth/login" className="btn btn-secondary">
                         Log In
                     </Link>
                     <Link to="/auth/signup" className="btn btn-primary">
-                        Sign Up
+                        Get Started
                     </Link>
-                </>
-            )}
-            {isAuthenticated && (
-                <>
-                    <button
-                        className="btn btn-secondary"
-                        onClick={async () => {
-                            await logout();
-                            setIsAuthenticated(false);
-                            navigate("/");
-                        }}
-                    >
-                        Log Out
-                    </button>
                 </>
             )}
         </div>
     );
 }
 
-export default NavLinks;
+export default AuthMenu;
--- frontend/src/components/Navbar.tsx
+++ frontend/src/components/Navbar.tsx
@@ -1,15 +1,28 @@
-import { NAVBAR_HEIGHT } from "../utils/constants";
+import AuthMenu from "./AuthMenu";
 import BrandLogo from "./BrandLogo";
-import NavLinks from "./NavLinks";
+import DesktopMenu from "./DesktopMenu";
+import MobileMenu from "./MobileMenu";
 
 function Navbar(): React.ReactNode {
     return (
-        <nav
-            className="bg-base-100 shadow-md p-4 md:px-8 flex justify-between align-middle"
-            style={{ height: NAVBAR_HEIGHT }}
-        >
-            <BrandLogo />
-            <NavLinks />
+        <nav className="navbar bg-base-100 shadow-sm">
+            <div className="navbar-start gap-2">
+                <div className="dropdown">
+                    <MobileMenu />
+                </div>
+                <BrandLogo
+                    enableLinkToHomeLogo={true}
+                    enableLinkToHomeText={true}
+                />
+            </div>
+
+            <div className="navbar-center hidden md:flex">
+                <DesktopMenu />
+            </div>
+
+            <div className="navbar-end">
+                <AuthMenu />
+            </div>
         </nav>
     );
 }
