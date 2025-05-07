51c90e4ba8a90ad54a837247792825ac63a1d5b3
dmarts05 <dmarts05@estudiantes.unileon.es>
2025-05-07 17:34:52 +0000

    feat: add blur to space in post image

--- frontend/src/components/post-card/PostCardImage.tsx
+++ frontend/src/components/post-card/PostCardImage.tsx
@@ -20,18 +20,30 @@ function PostCardImage({
         <>
             <figure
                 onClick={openModal}
-                className="border-y border-base-200 cursor-pointer"
+                className="relative border-y border-base-200 cursor-pointer overflow-hidden"
+                style={{ height: `${height}px` }}
             >
+                {/* Blurred background layer */}
+                <div
+                    className="absolute inset-0 bg-cover bg-center blur-md scale-x-150"
+                    style={{
+                        backgroundImage: `url(${String(imageUrl)})`,
+                    }}
+                />
+
+                {/* Foreground image with white background */}
                 <img
                     src={String(imageUrl)}
                     alt={imageAlt}
+                    className="relative mx-auto bg-white border-r-1 border-l-1 border-base-200"
                     style={{
-                        minWidth: `${height}px`,
-                        minHeight: `${height}px`,
+                        minWidth: height,
+                        minHeight: height,
                     }}
                 />
             </figure>
 
+            {/* Modal dialog for full image */}
             <dialog id={dialogId} className="modal">
                 <div className="modal-box p-0">
                     <img
